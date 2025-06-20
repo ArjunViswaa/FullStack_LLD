12bcbfb1a70d43325662a0cd53b2908fc006b0f8
Arjun Viswaa <arjun.viswanath@rentokil-initial.com>
2025-06-20 22:13:16 +0530

    Initial for Templating design

--- FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/app.js
+++ FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const path = require("path");
 
 const app = express();
+app.set('view engine', 'pug');
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
@@ -32,7 +33,7 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminRoutes.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/routes/admin.js
+++ FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/routes/admin.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const router = express.Router();
+const products = [];
 
 router.get("/add-product", (req, res, next) => {
     // res.send("<form action='/admin/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
@@ -9,8 +10,11 @@ router.get("/add-product", (req, res, next) => {
 });
 
 router.post("/product", (req, res, next) => {
-    console.log(req.body);
+    // console.log(req.body);
+    products.push({ title: req.body.title });
+    console.log("prods", products);
     res.redirect("/");
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
