a5b128caf3b8b1580797fbbc2b906db422787e31
Arjun Viswaa <arjun.viswanath@rentokil-initial.com>
2025-06-21 07:38:51 +0530

    Additional Templating engines commits

--- FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/app.js
+++ FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/app.js
@@ -1,52 +1,24 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require("path");
 
 const app = express();
-app.set('view engine', 'pug');
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
 
-// app.use((req, res, next) => {
-//     console.log("In the middleware");
-//     next(); // Allows the request to continue to the next middleware in line
-// });
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-// app.use((req, res, next) => {
-//     console.log("In another middleware");
-//     res.send('<h1>Hello from Express</h1>');
-// });
-
-// Moved to routes/admin.js
-// app.use("/add-product", (req, res, next) => {
-//     res.send("<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
-// });
-
-// app.post("/product", (req, res, next) => {
-//     console.log(req.body);
-//     res.redirect("/");
-// });
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/admin", adminRoutes.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-})
-
-// Moved to routes/shop.js
-// app.use("/", (req, res, next) => {
-//     console.log("In the middleware");
-//     res.send('<h1>Hello from Express</h1>');
-// });
-
-// const server = http.createServer(app);
-// server.listen(3000);
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
--- FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/routes/admin.js
+++ FullStack_LLD_4-Node JS_Express_MongoDB/Maxmilian/Express/routes/admin.js
@@ -1,19 +1,28 @@
-const express = require("express");
-const path = require("path");
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
 
 const router = express.Router();
+
 const products = [];
 
-router.get("/add-product", (req, res, next) => {
-    // res.send("<form action='/admin/product' method='POST'><input type='text' name='title'><button type='submit'>Add Product</button></form>");
-    res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+// /admin/add-product => GET
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+    formsCSS: true,
+    productCSS: true,
+    activeAddProduct: true
+  });
 });
 
-router.post("/product", (req, res, next) => {
-    // console.log(req.body);
-    products.push({ title: req.body.title });
-    console.log("prods", products);
-    res.redirect("/");
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
 });
 
 exports.routes = router;
