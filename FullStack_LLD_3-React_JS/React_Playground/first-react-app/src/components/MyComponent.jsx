9c4ac36d06318e97d9032c7da0e19b0bb94d986d
Arjun Viswaa <arjun.viswanath@rentokil-initial.com>
2025-07-17 20:41:09 +0530

    React JS Playground commit

---
+++ FullStack_LLD_3-React_JS/React_Playground/first-react-app/src/components/MyComponent.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>Hello from My Component</h1>
+    </div>
+  )
+};
+
+export const AnotherComponent = () => {
+    return (
+    <div>
+        <h1>Hello from My Another Component</h1>
+    </div>
+  )
+}
+
+export default MyComponent;
